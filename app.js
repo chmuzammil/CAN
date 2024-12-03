93f4e8fff1654fea9e8255d523a6b3b6e70edac9
chmuzammil <chmuzammilhassan@gmail.com>
2024-12-04 00:45:36 +0500

    initial-2

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
+const image = document.querySelector('.p-image');
+const lenis = new Lenis()
+
+function raf(time){
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+};
+requestAnimationFrame(raf);
+
 var tl= gsap.timeline({
     scrollTrigger:{
         trigger:".two",
         start: "0% 95%",
         end:"50% 50%",
+       
         scrub: true,
-        
+        onUpdate: function() {
+            // Dynamically calculate blur based on velocity
+            const progress = this.progress(); // Progress of the animation (0 to 1)
+            const blurAmount = Math.abs(progress - 0.5) * 10; // Simulate motion blur effect
+            image.style.filter = `blur(${blurAmount}px)`;
+          },
+          onComplete: function() {
+            // Reset blur at the end of animation
+            image.style.filter = 'none';
+          }
 
     }
 })
